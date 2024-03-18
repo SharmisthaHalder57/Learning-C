4c17295e51a8df4e1ee9ef3ce2737f2c507cb6a2
sharmistha2335 <150144062+SharmisthaHalder57@users.noreply.github.com>
2024-03-18 20:30:40 +0530

    My first learning

---
+++ codechef/practice/LBCL03_problems_CLB013.c
@@ -0,0 +1,20 @@
+// Inserting space between outputs
+// You can also have multiple outputs together in a single "printf()" function.
+
+// For example:
+
+// printf("%d %d", 2, 3);
+
+// // Output:
+// // 2 3
+// We can have as many %d as we want. Because we added a space between two %d, the output has space between two numbers.
+
+#include <stdio.h>
+
+int main() {
+
+    // Add two %d to print two numbers
+    printf("%d %d", 7, 3);
+	
+}
+
