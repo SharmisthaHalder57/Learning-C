cb81d0b210478169dcc149cbc3cf6798365baf42
sharmistha2335 <150144062+SharmisthaHalder57@users.noreply.github.com>
2024-03-18 23:32:44 +0530

    My first code

---
+++ codechef/course/LBCL13_problems_CLB067.c
@@ -0,0 +1,61 @@
+// Else If Statement
+// In cases where you have to check for multiple conditions and run some code based on each, you have to use else if.
+
+// The else if keyword means "if the previous conditions were not true, then try this condition"
+
+// The following example illustrates usage of else if.
+
+// grade = 85;
+// if (grade >= 90) {
+//     printf("You got an A");
+// } else if (grade >= 80) {
+//     printf("You got a B");
+// }
+
+// // Output
+// // You got a B
+// The code above works as follows
+
+// If grade >= 90, then it will output: You got an A
+// If grade is between 80 and 90 - it will output: You got a B
+// If grade is less than 80 - there will be no output
+// Task
+// Write a program which does the following
+
+// Take two integers b and r as input
+// Print "Rob scored higher marks than Bob", if r is greater than b
+// Print "Bob & Rob both scored the same", if both b and r are equal
+// Sample 1:
+// Input
+// Output
+// 20
+// 25
+// Rob scored higher marks than Bob
+// Sample 2:
+// Input
+// Output
+// 15
+// 15
+// Bob & Rob both scored the same
+
+
+#include <stdio.h>
+
+int main() {
+    int b, r;
+    scanf("%d", &b);
+    scanf("%d", &r);
+    
+    if(b > r){
+        printf("Bob scored higher marks than Rob");
+    }
+    else if(r > b){
+        printf("Rob scored higher marks than Bob");
+    }
+    else{
+        printf("Bob & Rob both scored the same");
+    }
+    
+    return 0;
+}
+
