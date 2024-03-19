a57f6bcd8299240aeab113188b8228729c0df094
sharmistha2335 <150144062+SharmisthaHalder57@users.noreply.github.com>
2024-03-19 14:25:44 +0530

    learning c

---
+++ codechef/course/LBCL05_problems_CLB022.c
@@ -0,0 +1,12 @@
+// A variable is like a labeled box where you can store data. Imagine you have a box labeled "age" and you put the number 25 in it. In C, you would do this by writing int age = 25;. This means you're storing the number 25 in the variable named age.
+
+// int denotes that we will be storing integers (numbers) in this variable.
+
+// // Here's the cool part: whenever you use age in your code, C will remember it is 25. For example, if you write printf("%d", age), C will show 25
+#include <stdio.h>
+
+int main() {
+    // Replace __ with 25
+    int age = 25;
+    printf("%d", age);
+}
